b0c5e4a4ee7f1a8e229b5d716bbc341a0e960800
Wadie67 <e8.intern.44@gmail.com>
2026-09-09 15:16:45 +0800

    feat: add input and response

--- main.go
+++ main.go
@@ -2,13 +2,29 @@ package main
 import(
 	"fmt"
 	"time"
+	"os"
+	"bufio"
 )
 
 func main() {
+
+	scanner := bufio.NewScanner(os.Stdin)
 	today := time.Now()
 	date := today.Weekday()
+
 	fmt.Println("Hello Wadie!")
 	fmt.Println(" ")
 	fmt.Println("Today is a", date)
 	fmt.Println("What would you like to do today?")
+
+	for scanner.Scan() {
+		input := scanner.Text()
+
+		fmt.Println("You want to:", input)
+
+			if input == "quit" {
+				break
+			}
+	}
+
 }

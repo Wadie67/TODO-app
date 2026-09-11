e9fd2f07bbf634e79aaa6e2d9adc55921f225c55
Wadie67 <e8.intern.44@gmail.com>
2026-09-11 14:38:21 +0800

    feat: add "add" function

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
-import(
-	"fmt"
-	"time"
-	"os"
+
+import (
 	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"time"
 )
 
 func main() {
@@ -17,13 +19,13 @@ func main() {
 	fmt.Println("Today is a", date)
 	fmt.Println("What would you like to do today?")
 	fmt.Print("> ")
+loop:
 	for scanner.Scan() {
 		input := scanner.Text()
-		if input == "quit" {
-			fmt.Println("Goodbye!")
-				break
-			}
-		switch input {
+		parts := strings.SplitN(input, " ", 2)
+		switch parts[0] {
+		default:
+			fmt.Println("You want to:", parts[1])
 		case "-help":
 			fmt.Println("Available commands:")
 			fmt.Println(" ", "add <task>")
@@ -33,9 +35,48 @@ func main() {
 			fmt.Println(" ", "clear")
 			fmt.Println(" ", "help")
 			fmt.Println(" ", "quit")
-		default:
-			fmt.Println("You want to:", input)
+		case "add":
+			if len(parts) < 2 {
+				fmt.Println("Give me a task!")
+			} else {
+				name := parts[1]
+				createtodo(scanner, name)
+			}
+		case "quit":
+			fmt.Println("Goodbye!")
+			break loop
 		}
 		fmt.Print("> ")
+	}
+}
+func createtodo(scanner *bufio.Scanner, name string) todo {
+
+	fmt.Println("What is the Priority of this Task?")
+	fmt.Print("> ")
+	scanner.Scan()
+	priority := scanner.Text()
+	fmt.Println("You entered:", priority)
+
+	fmt.Println("When would you like this Task done?")
+	var deadline time.Time
+	for {
+		fmt.Print("> ")
+		scanner.Scan()
+		deadlineinput := scanner.Text()
+		deadline, err := time.Parse("3:05pm", deadlineinput)
+		if err != nil {
+			fmt.Println("Not a valid time vro</3")
+			continue
+		} else {
+			fmt.Println("You entered:", deadline.Format("3:05pm"))
+				fmt.Println("Successfully listed task, What else do you want")
+			break
+		}
+	}
+		return todo{
+		name:      name,
+		priority:  priority,
+		deadline:  deadline,
+		completed: false,
 	}
 }
